67d7bd1ed53d41447dfd2784aa9d1ad4fdf274a1
shuangdudu <2353773984@qq.com>
2018-03-07 12:14:19 +0800

    ..

--- src/main/java/com/gsstudy/it/dao/DemoDao.java
+++ src/main/java/com/gsstudy/it/dao/DemoDao.java
@@ -1,6 +1,9 @@
 package com.gsstudy.it.dao;
 
+import java.util.List;
 import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
 
 import com.gsstudy.it.entity.ProcedurePojo;
 
@@ -8,4 +11,12 @@ import com.gsstudy.it.entity.ProcedurePojo;
 
 public interface DemoDao {
 	public void count(ProcedurePojo pojo);
+	public List<Map<String,Object>> findParameterValue(
+			@Param("boqID")String boqID,
+			@Param("moduleCodes")List<String>moduleCodes,
+			@Param("productCodes")List<String>productCodes,
+			@Param("parameterCode")String parameterCode,
+			@Param("siteName")String siteName,
+			@Param("dimensionProductTypeID")int dimensionProductTypeID,
+			@Param("qtyTpye")int qtyTpye);
 }
