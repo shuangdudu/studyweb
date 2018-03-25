899c1a5eb08afcc6ceba1c29d32f494f482b6df5
shuangdudu <2353773984@qq.com>
2018-03-25 18:51:48 +0800

    常量接口的使用

---
+++ src/main/java/com/gsstudy/it/common/AlgorithmConstant.java
@@ -0,0 +1,13 @@
+package com.gsstudy.it.common;
+
+public interface AlgorithmConstant {
+	interface CALC_SCOPE{
+		int BY_BOQ = 1;
+		int BY_SITE = 2;
+	}
+	interface QuoteMode{
+		//按模型报价
+		int QUOTE_BY_MODEL = 0;
+		int QUUTE_BY_SITE = 1;
+	}
+}
